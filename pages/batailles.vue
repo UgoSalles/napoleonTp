ecd61c322785b86a494e83283f6bd21eae8e3988
Salles ugo <ugo.salleslux@gmail.com>
2024-02-23 11:35:15 +0100

    fonction

--- pages/batailles.vue
+++ pages/batailles.vue
@@ -9,7 +9,7 @@ import type { BatailleInterface } from "~/types/bataille";
     
     const icons = defineModel<ListIcon>({required: true})
 
-    const passage = () => {
+    const passage = (batailleId: number) => {
         icons.value = icons.value === 'mdi-eye-arrow-left-outline' ? 'mdi-eye-arrow-left' : 'mdi-eye-arrow-left-outline'
     }
     const icon = computed(() =>
@@ -17,35 +17,86 @@ import type { BatailleInterface } from "~/types/bataille";
     )
 
     onMounted(() => {
-        retrieveSheetText()
+        //pour chaque bataille, calculer la longueur de la description et la couper si elle est trop longue
+        data.value.forEach((bataille) => {
+            cutDescription(bataille.id)
+        })
+
     })
-  
+    
+   
     
 
 
 
-    function retrieveSheetText() {
-        let sheet = document.getElementById('sheet')
-        if(sheet !== null){
-            sheet.textContent = sheet.textContent?.substring(0, 300)+'...';
-            
+   
+
+
+    // Référence pour stocker les descriptions des batailles
+    const descriptions = ref([] as { id: number; description: string }[]);
+
+
+
+    // couper la description a 300 caractère
+    const cutDescription = (batailleId: number) => {
+        const longDescription = data.value.find((item) => item.id === batailleId);
+        const lieu  = longDescription?.lieu.length
+        const force = longDescription?.force.length
+        const pertes = longDescription?.pertes.length
+        const totalLength = lieu! + force! + pertes!;
+
+        if (totalLength > 300) {
+            // Calculer le nombre de caractères à retirer de chaque attribut
+            const reductionFactor = 300 / totalLength;
+            const newLieuLength = Math.floor(lieu! * reductionFactor);
+            const newForceLength = Math.floor(force! * reductionFactor);
+            const newPertesLength = Math.floor(pertes! * reductionFactor);
+
+            // Réduire la longueur de chaque attribut en conséquence
+            const shortenedLieu = longDescription!.lieu.substring(0, newLieuLength);
+            const shortenedForce = longDescription!.force.substring(0, newForceLength);
+            const shortenedPertes = longDescription!.pertes.substring(0, newPertesLength);
+
+            // Construction de la nouvelle description
+            const shortenedDescription = `${shortenedLieu} ${shortenedForce} ${shortenedPertes}`;
+
+            // Mise à jour de l'élément HTML avec la nouvelle description
+            let sheet = document.getElementById('sheet');
+            if (sheet) {
+                sheet.textContent = shortenedDescription;
+            }
         }
+        
+        
     }
 
 
 
-    function handleClick(){
-        let sheet = document.getElementById('sheet')
-        let situation = document.getElementById('situation')
-        let sit = document.getElementById('sit')
 
-        if(sheet !== null){
-            sheet.textContent  = sheet.textContent?.slice(0, 900)+'...';
-            sit?.classList.toggle('situation')
-            situation?.classList.toggle('situation')
-            
+
+
+    // Référence pour stocker les descriptions étendues des batailles
+    const expandedDescriptions = ref([] as { id: number; elements: string[] }[]);
+
+    // Fonction pour vérifier si une description spécifique est étendue pour une bataille donnée
+    const isDescriptionExpanded = (batailleId: number, element: string) => {
+    const expanded = expandedDescriptions.value.find((item) => item.id === batailleId);
+    return expanded ? expanded.elements.includes(element) : false;
+    };
+
+    const handleClick = (batailleId: number) => {
+        const index = expandedDescriptions.value.findIndex((item) => item.id === batailleId);
+
+        if (index !== -1) {
+            // La bataille a déjà des détails étendus, donc les supprimer
+            expandedDescriptions.value.splice(index, 1);
+        } else {
+            // La bataille n'a pas de détails étendus, réinitialiser les autres à leur état initial
+            expandedDescriptions.value = [];
+            // Ajouter les nouveaux détails
+            expandedDescriptions.value.push({ id: batailleId, elements: ['lieu', 'force', 'pertes', 'situation'] });
         }
-    }
+    };
  
 
 
@@ -55,7 +106,7 @@ import type { BatailleInterface } from "~/types/bataille";
     
   <v-container class="container" v-for="bataille in data" :key="bataille.id" >
 
-    <VRow cols="12" sm="4" class="mainContainer" v-if="bataille.victoire" >
+    <VRow cols="12" sm="4" class="mainContainer" v-if="bataille.victoire"  >
         <VSheet class="entete" >
             <VLabel class="titre" :text="bataille.nom + ' - ' + bataille.annee"></VLabel>
             <VImg :src="'data:image/png;base64,'+ bataille.image" class="img" />
@@ -69,13 +120,13 @@ import type { BatailleInterface } from "~/types/bataille";
             <label>Pertes</label>
             <label class="text"> {{ bataille.pertes }}</label>
 
-            <label class="situation" id="sit">Situation générale</label>
-            <label class="text situation" id="situation" >{{bataille.situation}}</label>
+            <p v-if="isDescriptionExpanded(bataille.id, 'situation')" >Situation générale</p>
+            <p class="text" v-if="isDescriptionExpanded(bataille.id, 'situation')" >{{bataille.situation}}</p>
         </VSheet>
     
          
         <VSheet class="button" >
-            <VBtn  @click="handleClick()" @mouseover="passage()" @mouseout="passage()" :icon="icon" />
+            <VBtn  @click="handleClick(bataille.id)" @mouseover="passage(bataille.id)" @mouseout="passage(bataille.id)" :icon="icon" />
         </VSheet>
 
         
@@ -131,9 +182,7 @@ import type { BatailleInterface } from "~/types/bataille";
     width: 3%;
 }
 
-.situation{
-    display: none;
-}
+
 
 .text{
     margin-left: 30px;
